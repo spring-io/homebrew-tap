a224a3569bc8845e23a174545d6b97876e2886e3
Spring Builds <spring-builds@users.noreply.github.com>
2022-11-24 14:16:33 +0000

    Upgrade to Spring Boot 2.7.6

--- spring-boot.rb
+++ spring-boot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class SpringBoot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.7.5/spring-boot-cli-2.7.5-bin.tar.gz'
-  version '2.7.5'
-  sha256 'a34052b0da3946671a1805a1ce8a747d16e812a90cd19f1a59b4d1e812d4035b'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.7.6/spring-boot-cli-2.7.6-bin.tar.gz'
+  version '2.7.6'
+  sha256 '39f9edb89ad3c694cb1b6e7b111591101ba0c7cdc2901a8365289746b105bd52'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
