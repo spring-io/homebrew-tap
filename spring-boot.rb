a93704f009ad80e4afb3cfe66c3403b9e8692011
Spring Builds <spring-builds@users.noreply.github.com>
2022-12-22 23:57:47 +0000

    Upgrade to Spring Boot 3.0.1

--- spring-boot.rb
+++ spring-boot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class SpringBoot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/3.0.0/spring-boot-cli-3.0.0-bin.tar.gz'
-  version '3.0.0'
-  sha256 '70a7ec90fafdd637e5778662c94ef324a24e7fd193ff79ba7ffc0346596155b1'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/3.0.1/spring-boot-cli-3.0.1-bin.tar.gz'
+  version '3.0.1'
+  sha256 '58bfd79b4dc6913c45fc82f3f36e9ed52363963b21a2534106e33b4485618b52'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
