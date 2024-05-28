1554bc2c7e71b73dc5f33888260be2f1468810f5
Spring Builds <spring-builds@users.noreply.github.com>
2024-05-28 09:58:13 +0000

    Upgrade to Spring Boot 3.3.0

--- spring-boot.rb
+++ spring-boot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class SpringBoot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.2.6/spring-boot-cli-3.2.6-bin.tar.gz'
-  version '3.2.6'
-  sha256 '2613c25d591c28c222915f1563d64251690cffe1d9d00dd49c347099c9940dc0'
+  url 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.3.0/spring-boot-cli-3.3.0-bin.tar.gz'
+  version '3.3.0'
+  sha256 '334c0f1643ed1e61cf4c07d17abf46e9396e1c60f44c7912306859404598c25a'
   head 'https://github.com/spring-projects/spring-boot.git', :branch => "main"
 
   def install
