ccda311a17760c5583259b59f69f07a19e0e9b71
Spring Builds <spring-builds@users.noreply.github.com>
2024-07-19 07:40:23 +0100

    Correct the contents of spring-boot.rb for Boot 3.3.2 upgrade
    Fixes gh-7

--- spring-boot.rb
+++ spring-boot.rb
@@ -1,16 +1,24 @@
-<html>
-<head><title>404 Not Found</title></head>
-<body bgcolor="white">
-<center><h1>404 Not Found</h1></center>
-<hr><center>nginx</center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+require 'formula'
 
+class SpringBoot < Formula
+  homepage 'https://spring.io/projects/spring-boot'
+  url 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.3.2/spring-boot-cli-3.3.2-bin.tar.gz'
+  version '3.3.2'
+  sha256 '929d6a4b0eefd4ff4ab61dde2919b6aa5e20b485894b1894fb3078c469ab0f3c'
+  head 'https://github.com/spring-projects/spring-boot.git', :branch => "main"
 
+  def install
+    if build.head?
+      system './gradlew spring-boot-project:spring-boot-tools:spring-boot-cli:tar'
+      system 'tar -xzf spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions/spring-* -C spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions'
+      root = 'spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions/spring-*'
+    else
+      root = '.'
+    end
 
+    bin.install Dir["#{root}/bin/spring"]
+    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
+    zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
+  end
+end
