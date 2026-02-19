0bc8d3791eb4fccaaabae242d885af5039ed55ba
Spring Builds <spring-builds@users.noreply.github.com>
2026-02-19 13:59:41 +0000

    Upgrade to Spring Boot 4.0.3

--- spring-boot.rb
+++ spring-boot.rb
@@ -2,21 +2,12 @@ require 'formula'
 
 class SpringBoot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.5.11/spring-boot-cli-3.5.11-bin.tar.gz'
-  version '3.5.11'
-  sha256 'f59da63f877c89e25be8e39b481883565b98bcbec7516dfb0e0522889d39e8de'
-  head 'https://github.com/spring-projects/spring-boot.git', :branch => "main"
+  url 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/4.0.3/spring-boot-cli-4.0.3-bin.tar.gz'
+  version '4.0.3'
+  sha256 'c23198b768b25631ffd79076daa31a9c456dd7ae2df9511d296a504563b47549'
 
   def install
-    if build.head?
-      system './gradlew spring-boot-project:spring-boot-tools:spring-boot-cli:tar'
-      system 'tar -xzf spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions/spring-* -C spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions'
-      root = 'spring-boot-project/spring-boot-tools/spring-boot-cli/build/distributions/spring-*'
-    else
-      root = '.'
-    end
-
-    libexec.install Dir["#{root}/*"]
+    libexec.install Dir["./*"]
     (bin/"spring").write_env_script libexec/"bin/spring", {}
 
     bash_comp = libexec/"shell-completion/bash/spring"
